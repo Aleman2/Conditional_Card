a4d1f25b946e921a7072debe61030d89e83bfbb4
Kaiki <alejandrobisuteria@gmail.com>
2022-09-12 18:33:43 +0000

    Conditional.v1

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,62 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  var imagen = document.getElementById("imagen");
+
+  var imagen2 = imagen.files;
+  console.log(imagen2);
+  var primerArchivo = imagen2[0];
+  // Lo convertimos a un objeto de tipo objectURL
+  if (primerArchivo != null) {
+    const objectURL = URL.createObjectURL(primerArchivo);
+    variables.imagen = objectURL;
+    console.log(objectURL);
+  }
+
+  console.log("These are the current variables: ", variables);
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log(variables.imagen);
+  console.log("variables.imagen");
+  //
 
+  variables.instagram == null
+    ? variables.instagram == "https://instagram.com/4geeksacademy"
+    : "";
+  variables.twitter == null
+    ? variables.twitter == "https://twitter.com/4geeksacademy"
+    : "";
+  variables.github == null
+    ? variables.github == "https://github.com/4geeksacademy"
+    : "";
+  variables.linkedin == null
+    ? variables.linkedin == "https://linkedin.com/4geeksacademy"
+    : "";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <img src="${variables.imagen}" class="photo" />
+          <h1>${variables.name ? variables.name : "Put your name here"}, ${
+    variables.lastname ? variables.lastname : "Put your lastname here"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Choose your role"}</h2>
+          <h3>${variables.city ? variables.city : "Choose your city"}, ${
+    variables.country ? variables.country : "Choose your country"
+  }</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href=${
+              variables.twitter
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -68,7 +84,8 @@ window.onload = function() {
     lastname: null,
     role: null,
     country: null,
-    city: null
+    city: null,
+    imagen: null
   };
   render(window.variables); //render the card for the first time
 
